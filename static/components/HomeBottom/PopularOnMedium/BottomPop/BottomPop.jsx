06a64671c7968150f8f17c8be6cc74c8168f737d
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-12-30 23:47:07 -0500

    added stick to popular section

---
+++ static/components/HomeBottom/PopularOnMedium/BottomPop/BottomPop.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './BottomPop.css';
+
+export default function BottomPop() {
+   return (
+      <div className='BottomPop'>
+         <ul>
+            <li>
+               <a>Help</a>
+            </li>
+            <li>
+               <a>Status</a>
+            </li>
+            <li>
+               <a>Writer</a>
+            </li>
+            <li>
+               <a>Blog</a>
+            </li>
+            <li>
+               <a>Careers</a>
+            </li>
+         </ul>
+      </div>
+   );
+}
--- static/components/HomeBottom/PopularOnMedium/PopularOnMedium.jsx
+++ static/components/HomeBottom/PopularOnMedium/PopularOnMedium.jsx
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from 'react';
 import PopContainer from './PopContainer/PopContainer';
 import axios from 'axios';
 import './PopularOnMedium.css';
+import BottomPop from './BottomPop/BottomPop';
+
+function stickyBottom(component, height) {
+   if (window.pageYOffset >= height) {
+      component.classList.add('sticky-2');
+   } else {
+      component.classList.remove('sticky-2');
+   }
+}
+
+function handleScroll() {
+   const component = document.getElementById('PopularOnMedium');
+   stickyBottom(component, 570);
+}
 
 export default function PopularOnMedium() {
    const [topArticles, setTop] = useState([]);
@@ -13,14 +27,22 @@ export default function PopularOnMedium() {
          }
          getTopArticles();
       }
-   }, topArticles);
+   }, [topArticles]);
+   useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+         window.removeEventListener('scroll', handleScroll);
+      };
+   }, [handleScroll]);
 
    return (
-      <div className='container PopularOnMedium'>
+      <div className='container PopularOnMedium' id='PopularOnMedium'>
          <h3 id='title-text'>Popular On Medium</h3>
          {topArticles.map((article, idx) => (
             <PopContainer {...article} idx={idx} />
          ))}
+         <BottomPop />
       </div>
    );
 }
